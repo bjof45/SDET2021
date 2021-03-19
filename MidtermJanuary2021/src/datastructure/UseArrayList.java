99b34ba0319fadc3ac75366f0c53dc8a7756d433
bjof45 <bjof45@gmail.com>
2021-03-19 17:27:26 -0400

    did array portion, still need to add to database

--- MidtermJanuary2021/src/datastructure/UseArrayList.java
+++ MidtermJanuary2021/src/datastructure/UseArrayList.java
@@ -1,4 +1,8 @@
 package datastructure;
+//BR
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class UseArrayList {
 
@@ -9,7 +13,26 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
-	
+		//Sprint,T-Mobile,ATT,Verizon
+		ArrayList<String> phoneCompany = new ArrayList<String>();
+		Scanner getInput = new Scanner(System.in);
+		String tempString;
+
+		System.out.print("Enter a couple of phone companies separated by a comma: ");
+		tempString = getInput.nextLine();
+
+		phoneCompany = new ArrayList<>(Arrays.asList(tempString.split(",")));
+
+		for(String word : phoneCompany)
+			System.out.println("Phone company provider: "+word);
+
+		System.out.println("");
+
+		if(phoneCompany.remove("Sprint")) {
+			System.out.println("My phone company Sprint was removed\n");
+		}
+		for(String word : phoneCompany)
+			System.out.println("Phone company provider: "+word);
 
 	}
 
