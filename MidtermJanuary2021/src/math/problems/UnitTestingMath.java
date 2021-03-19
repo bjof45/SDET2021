cfb21b502ce8a737168b03bfd053d2057b246777
bjof45 <bjof45@gmail.com>
2021-03-19 12:36:46 -0400

    started unit testing for math.problems

--- MidtermJanuary2021/src/math/problems/UnitTestingMath.java
+++ MidtermJanuary2021/src/math/problems/UnitTestingMath.java
@@ -1,8 +1,16 @@
 package math.problems;
 
+import org.testng.Assert;
+
 public class UnitTestingMath {
     public static void main(String[] args) {
-        //Apply Unit testing into each classes and methods in this package.
+       // Test factorial
+
+        int result1 = math.problems.Factorial.factorialIteration(5);
+        int result2 = math.problems.Factorial.factorialRecursion(5);
+
+        Assert.assertEquals(result1, 120);
+        Assert.assertEquals(result2, 120);
 
 
 
