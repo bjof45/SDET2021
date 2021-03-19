571f419c5e20fd80fda26b86269fea594e84ff1e
bjof45 <bjof45@gmail.com>
2021-03-19 16:52:17 -0400

    finished queue

--- MidtermJanuary2021/src/datastructure/UseQueue.java
+++ MidtermJanuary2021/src/datastructure/UseQueue.java
@@ -1,5 +1,6 @@
 package datastructure;
-
+//BR
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -11,9 +12,42 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+		String tempString;
+
 		Queue<String> queue = new LinkedList<>();
-		queue.poll();
 
-	}
+		queue.add("Jay-Z");
+		queue.add("Benny The Butcher");
+		queue.add("Conway the Machine");
+		queue.add("WestSide Gunn");
+		queue.add("Rick Ross");
 
+		Queue<String> cqueue = new LinkedList<>();
+
+
+		System.out.println("Queue is filled with my favorite rappers, the 1st would be "+queue.peek());
+		tempString = queue.remove();
+
+		System.out.println("Next up in my list is "+queue.peek());
+		System.out.println("Adding '"+tempString+"' back to this queue, where will it land?");
+		queue.add(tempString);
+
+		System.out.println("The front is now: "+queue.peek()+" [expected result is '"+tempString+"' being added to end of queue]");
+		System.out.println("Without further ado-");
+
+		//while loop
+		while((tempString = queue.poll()) != null) {
+			System.out.println("A favorite rapper of mine is " + tempString);
+			cqueue.add(tempString); // add to our 2nd queue
+		}
+
+
+		//iterator
+		Iterator<String> it = cqueue.iterator();
+
+		while(it.hasNext())
+			System.out.println("IT: "+it.next());
+
+		}
 }
+
