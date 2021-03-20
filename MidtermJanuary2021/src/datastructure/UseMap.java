e155de90532633f9fe35e0ea842ec9f8435369ab
bjof45 <bjof45@gmail.com>
2021-03-19 20:29:02 -0400

    finished UseMap

--- MidtermJanuary2021/src/datastructure/UseMap.java
+++ MidtermJanuary2021/src/datastructure/UseMap.java
@@ -1,5 +1,11 @@
 package datastructure;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -10,6 +16,48 @@ public class UseMap {
 		 *
 		 */
 
-	}
+		HashMap<String,String> myMap = new HashMap<>();
+		Map<String, List<String>> list = new HashMap<String, List<String>>();
+
+		myMap.put("Batman", "DC");
+		myMap.put("Superman", "DC");
+		myMap.put("Flash", "DC");
+		myMap.put("Captain Marvel", "DC");
+		myMap.put("Cyclops", "Marvel");
+		myMap.put("Professor X", "Marvel");
+		myMap.put("Spider-Man", "Marvel");
+		myMap.put("Captain America", "Marvel");
+
+		List<String> villainsDC = new ArrayList<>();
+		List<String> villainsMarvel = new ArrayList<>();
+
+		villainsDC.add("Joker");
+		villainsDC.add("Lex Luthior");
+		villainsDC.add("Cheetah");
+		villainsDC.add("Black Adam");
+
+		villainsMarvel.add("Dr.Oct");
+		villainsMarvel.add("Red Skull");
+		villainsMarvel.add("Magneto");
+		villainsMarvel.add("Mr.Sinister");
+
+		for(Entry<String,String> entry: myMap.entrySet()) {
+			System.out.println(entry.getKey()+" from "+entry.getValue());
+
+			if(entry.getValue().equals("DC")) {
+				list.put(entry.getKey(), villainsDC);
+			} else {
+				list.put(entry.getKey(), villainsMarvel);
+			}
+		}
+
+		System.out.println("--------------------------");
+		
+		for(Entry<String, List<String>> entry: list.entrySet()) {
+			System.out.println("Character "+entry.getKey()+" has a list of villains such as "+entry.getValue());
+
+		}
+		}
 
 }
+
