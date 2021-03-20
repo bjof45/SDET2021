308753cbcdf473ebad92085eef3c02c59fc33fb8
bjof45 <bjof45@gmail.com>
2021-03-20 15:42:03 -0400

    fixed to use 'return'

--- MidtermJanuary2021/src/math/problems/Fibonacci.java
+++ MidtermJanuary2021/src/math/problems/Fibonacci.java
@@ -6,35 +6,37 @@ public class Fibonacci {
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
-createFibonacci(40);
+System.out.println(createFibonacci(40));
 
 
     }
     // ArrayList<Integer>
-    public static void createFibonacci(int count) {
-        int tempCount = 2; // we automatically start out with 0,1 , wasn't sure if we counted those or not?
+    public static String createFibonacci(int count) {
+        int tempCount = 1; // we automatically start out with 0,1 , wasn't sure if we counted those or not?
         int tempNumber1 = 0;
         int tempNumber2 = 1;
         int tempNumber3 = 0;
+        String tempString = "";
         tempNumber3 = tempNumber1+tempNumber2;
-        System.out.print(tempNumber1+","+tempNumber2+","+tempNumber3);
+        tempString = tempString + tempNumber1+","+tempNumber2+","+tempNumber3;
 
         do {
 
             tempCount++;
 
-            if(tempCount != count)
-                 System.out.print(",");
+            if(tempCount != count) {
+                tempString = tempString+",";
+            }
             else
-                break;
+                 break;
 
             tempNumber1 = tempNumber2;
             tempNumber2 = tempNumber3;
             tempNumber3 = tempNumber1+tempNumber2;
-
-            System.out.print(tempNumber3);
+            tempString = tempString+tempNumber3;
 
         } while(tempCount <= count);
+    return tempString;
     }
 
 }
